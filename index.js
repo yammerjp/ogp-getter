2f3edfe6ffc7fe0599f573ae9ae19da918cc38d7
Keisuke Nakayama <baskk4.dt@gmail.com>
2020-09-19 23:12:32 +0900

    Add endpoints to get meta-tags

---
+++ index.js
@@ -0,0 +1,28 @@
+`use strict`
+
+const fetch = require('node-fetch');
+const { parse } = require('node-html-parser');
+
+const express = require('express');
+const app = express();
+
+app.get('/', async (req,res) => {
+  res.status(200).json({
+    live:true,
+    message: "The API server is living!"
+  }).end();
+});
+
+app.get('/metas', async (req,res) => {
+  const url = req.query.url;
+
+  const fetchedText = await fetch(url).then(res=>res.text());
+  const root = parse(fetchedText);
+  const metas = root.querySelectorAll("meta");
+  const metajson = metas.map(meta => ({meta: meta.rawAttrs}));
+
+  res.status(200).json({url, metas: metajson}).end();
+});
+
+const port = process.env.PORT || 3000;
+app.listen(3000, () => console.log(`listening on port ${port}`));
