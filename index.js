b780298db6bc14fd3686e9b8df67c16393105006
Keisuke Nakayama <baskk4.dt@gmail.com>
2020-09-22 02:46:08 +0900

    Refactoring

--- index.js
+++ index.js
@@ -6,6 +6,7 @@ const { parse } = require('node-html-parser');
 const express = require('express');
 const app = express();
 
+// $ curl http://localhost:3000/
 app.get('/', async (req,res) => {
   res.status(200).json({
     live:true,
@@ -13,45 +14,57 @@ app.get('/', async (req,res) => {
   }).end();
 });
 
-app.get('/meta', async (req,res) => {
-  const url = req.query.url;
-
-  const fetchedText = await fetch(url).then(res=>res.text());
-  const root = parse(fetchedText);
-  const metas = root.querySelectorAll("meta");
-  const metajson = metas.map(meta => ({meta: meta.rawAttrs}));
-
-  res.status(200).json({url, metas: metajson}).end();
-});
-
-app.get('/ogp', async (req,res) => {
-  const url = req.query.url;
-
+const createMetaStrings = async (url) => {
   const fetchedText = await fetch(url).then(res=>res.text());
   const root = parse(fetchedText);
   const metas = root.querySelectorAll("meta");
   const metaStrings = metas.map(meta => meta.rawAttrs);
+  return metaStrings;
+};
 
-  const regex = /^property=('|")og:[a-z_]+('|")( )+content=('|").*('|")$/;
+// $ curl http://localhost:3000/meta?url=https://basd4g.net/
+app.get('/meta', async (req,res) => {
+  const url = req.query.url
 
-  const ogpStrings = metaStrings.
-    map( s => s.trim() ).
-    filter( s => regex.test(s)
-  );
-  const ogp = ogpStrings.map( ogpString => {
-    const propLength= "property=".length
-    const colonOrSemicolon = ogpString[propLength];
-    const property = ogpString.slice(
-      propLength + colonOrSemicolon.length
-    ).split(colonOrSemicolon, 2)[0];
-
-    const contentWithSeparator = ogpString.split("content=", 2)[1];
-    const content = contentWithSeparator.slice(1, -1);
-
-    return { property, content };
+  createMetaStrings(url).then( meta => {
+    res.status(200).json({url, meta}).end();
+  }).catch( () => {
+    res.status(500).end();
   });
+});
 
-  res.status(200).json({url, ogp}).end();
+const ogpString2ogpObject = ogpString => {
+  const propLength= "property=".length
+  const colonOrSemicolon = ogpString[propLength];
+  const property = ogpString.slice(
+    propLength + colonOrSemicolon.length
+  ).split(colonOrSemicolon, 2)[0];
+
+  const contentWithSeparator = ogpString.split("content=", 2)[1];
+  const content = contentWithSeparator.slice(1, -1);
+
+  return { property, content };
+};
+
+// $ curl http://localhost:3000/ogp?url=https://basd4g.net/
+app.get('/ogp', async (req,res) => {
+  const url = req.query.url
+
+  createMetaStrings(url).then( metaStrings => {
+
+    const regex = /^property=('|")og:[a-z_]+('|")( )+content=('|").*('|")$/;
+
+    const ogpStrings = metaStrings.
+      map( s => s.trim() ).
+      filter( s => regex.test(s)
+    );
+
+    const ogp = ogpStrings.map(ogpString2ogpObject);
+
+    res.status(200).json({url, ogp}).end();
+  }).catch( () => {
+    res.status(500).end();
+  });
 });
 
 const port = process.env.PORT || 3000;
